964bef1a6d5a09a3e21324e3918240d5a8023124
Matis DENE <matis.dene@edu.devinci.fr>
2024-05-14 15:11:49 +0200

    archi update

--- functions/hotelGet/index.js
+++ functions/hotelGet/index.js
@@ -1,21 +1,7 @@
-const fs = require("fs/promises");
 const functions = require('@google-cloud/functions-framework');
 
-function listHotels() {
-  //   return 3;
-  return new Promise((resolve, reject) => {
-    fs.readFile("./hotels.json")
-      .then((data) => {
-        resolve(JSON.parse(data));
-      })
-      .catch((error) => {
-        reject(0); // Reject the promise if there's an error
-      });
-  });
-}
 
 functions.http('helloGET', (req, res) => {
   res.send('Hello World!');
 });
 
-module.exports = listHotels;
