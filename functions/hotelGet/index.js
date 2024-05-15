89eff9cd48055fcf98ffed0aa4ecee8b9eb88c87
Arthur <arthurlucas92@orange.fr>
2024-05-15 14:05:06 +0200

    index.js edited

--- functions/hotelGet/index.js
+++ functions/hotelGet/index.js
@@ -16,7 +16,10 @@ function listHotels() {
 module.exports = listHotels;
 
 functions.http("helloGET", (req, res) => {
-  res.send(JSON.stringify(listHotels()));
+  const hotels = listHotels();
+  const hotelNames = Object.values(hotels).join("; ");
+  const htmlResponse = `<p style="text-align: center; font-family :poppins;">${hotelNames}</p>`;
+  res.send(htmlResponse);
   // fs.readFile("./hotels.json")
   //   .then((data) => {
   //     const hotels = JSON.parse(data);
