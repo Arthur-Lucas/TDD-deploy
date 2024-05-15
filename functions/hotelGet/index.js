3998d641855f657c0e11b09fade994468e20d5dc
Arthur <arthurlucas92@orange.fr>
2024-05-15 15:05:27 +0200

    functionnal test added

--- functions/hotelGet/index.js
+++ functions/hotelGet/index.js
@@ -17,32 +17,44 @@ function listHotels() {
 module.exports = listHotels;
 
 functions.http("hotelGET", async (req, res) => {
-  try {
-    const hotels = await listHotels();
-    if (hotels === 0) {
-      throw new Error("Failed to load hotels");
-    }
-    const hotelNames = Object.values(hotels).join("; ");
-    const htmlResponse = `<p style="text-align: center; font-family: poppins;">${hotelNames}</p>`;
-    res.send(htmlResponse);
-  } catch (error) {
-    res.status(500).send("An error occurred while fetching hotel data");
+  const pathName = req.path;
+  switch (pathName) {
+    case "/":
+    case "/hotelList":
+      try {
+        const hotels = await listHotels();
+        if (hotels === 0) {
+          throw new Error("Failed to load hotels");
+        }
+        const hotelNames = Object.values(hotels).join("; ");
+        const htmlResponse = `<p style="text-align: center; font-family: poppins;">${hotelNames}</p>`;
+        res.status(200).send(htmlResponse);
+      } catch (error) {
+        res.status(500).send("An error occurred while fetching hotel data");
+      }
+      break;
+    case "/healt":
+      res.status(204).send("Healt page");
+      break;
+    default:
+      res.status(404).send("Not Found");
+      break;
   }
 });
 
-functions.http("hotelTEST", async (req, res) => {
-  try {
-    const hotels = await listHotels();
-    if (hotels === 0) {
-      throw new Error("Failed to load hotels");
-    }
-    const hotelNames = Object.values(hotels).join(", ");
-    const htmlResponse = `${hotelNames}`;
-    res.send(htmlResponse);
-  } catch (error) {
-    res.status(500).send("An error occurred while fetching hotel data");
-  }
-});
+// functions.http("hotelTEST", async (req, res) => {
+//   try {
+//     const hotels = await listHotels();
+//     if (hotels === 0) {
+//       throw new Error("Failed to load hotels");
+//     }
+//     const hotelNames = Object.values(hotels).join(", ");
+//     const htmlResponse = `${hotelNames}`;
+//     res.send(htmlResponse);
+//   } catch (error) {
+//     res.status(500).send("An error occurred while fetching hotel data");
+//   }
+// });
 
 // fs.readFile("./hotels.json")
 //   .then((data) => {
